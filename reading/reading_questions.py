b82b7b277f7662e8432d08ee9fa6057a4758aa00
Vlad20055 <maksimenkovvlad111@mail.ru>
2025-04-27 01:47:37 +0300

    Implement Reading version: Vlad_v1

--- reading/reading_questions.py
+++ reading/reading_questions.py
@@ -1,25 +1,17 @@
+questions = [
+    {"text": """1. According to the passage, which of the following is true about faunal association?
+     (A) It estimates the time period of when humans first existed
+     (B) It examines animals that lived 3,000 to 4,000 years ago
+     (C) It mainly studies bison in the southwestern United States
+     (D) It finds out when a species lived by looking at its remains""",
+     "options": ["A", "B", "C", "D"],
+     "correct": 3},
 
-
-reading_questions = [
-    {
-        'question': 'Текст вопроса 1...',
-        'options': [
-            'Вариант ответа 1',
-            'Вариант ответа 2',
-            'Вариант ответа 3',
-            'Вариант ответа 4'
-        ],
-        'correct_answer': 0  # Индекс правильного ответа
-    },
-    {
-        'question': 'Текст вопроса 2...',
-        'options': [
-            'Вариант ответа 1',
-            'Вариант ответа 2',
-            'Вариант ответа 3',
-            'Вариант ответа 4'
-        ],
-        'correct_answer': 1
-    },
-    # ... остальные вопросы
+    {"text": """2. Why does the author mention bison in paragraph 2?
+      (A) To give an example of animals that live in the southwestern United States
+      (B) To contrast a dating method used on animals with one used on humans
+      (C) To show how archaeologists found out when humans first lived in Folsom
+      (D) To illustrate how people hunted and killed animals thousands of years ago""",
+     "options": ["A", "B", "C", "D"],
+     "correct": 2}
 ]
