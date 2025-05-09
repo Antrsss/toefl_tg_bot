3946c6c517038666d58fa9cdd21bc62215ce48a5
Darya Zgirskaya <129535397+Antrsss@users.noreply.github.com>
2025-05-09 22:28:20 +0300

    Listening questions (28) added

--- listening/listening_questions.py
+++ listening/listening_questions.py
@@ -1,25 +1,299 @@
-
-
-listening_questions = [
+l_questions = [
     {
-        'audio_file': 'listening/audio/jingle-bells.ogg',  # путь к файлу
-        'options': [
-            'Answer A',
-            'Answer B',
-            'Answer C',
-            'Answer D'
-        ],
-        'correct_answer': 2
+        "text": """1. Why is the student talking with the employee?
+          (A) To ask where Woolsey Hall is located
+          (B) To get advice on paying for a bill
+          (C) To find out how he can get a new student ID
+          (D) To complain about a missing student ID.""",
+        "audio_file": "listening/audio/18.01.ogg",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 2
     },
     {
-        'audio_file': 'listening/audio/jingle-bells.ogg',
-        'options': [
-            'Answer A',
-            'Answer B',
-            'Answer C',
-            'Answer D'
-        ],
-        'correct_answer': 1
+        "text": """2. Listen again to part of the conversation. Then answer the question. a Play Audio 18.02
+          (A) She wants to check the student's eyesight.
+          (B) She needs the student to help her find information.
+          (C) She is showing the student he came to the wrong ofice.
+          (D) She wants him to see the ID policies.""",
+        "audio_file": "listening/audio/18.02.ogg",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 3
     },
-    # добавляй столько вопросов, сколько нужно
+    {
+        "text": """3. What does the employee infer about the student?
+          (A) His parents don't pay for his bills
+          (B) He doesn't want to make his parents angry
+          (C) He frequently loses library books
+          (D) He is on the campus for the first time.""",
+        "audio_file": "",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 1
+    },
+    {
+        "text": """4. Where must the student go in order to pay for his ID?
+          (A) Bursar's office
+          (B) Forbes Avenue
+          (C) Student Accounts
+          (D) Student Union""",
+        "audio_file": "",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 0
+    },
+
+    #passage 2
+    {
+        "text": """5. What is the lecture mainly about?
+          (A) A comparison between animal and plant fossils
+          (B) The classification of how fossils form
+          (C) The process for creating trace fossils
+          (D) The origin of the museum's brontosaurus skeleton.""",
+        "audio_file": "listening/audio/18.03.ogg",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 1
+    },
+    {
+        "text": """6. Why does the professor ask the students to imagine walking across a muddy field?
+          (A) To illustrate how trace fossils form
+          (B) To contrast human and animal motion
+          (C) To emphasize how scientists use evidence
+          (D) To discuss the different types of fossilized footprints.""",
+        "audio_file": "",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 0
+    },
+    {
+        "text": """7. Listen again to part of the lecture. Then answer the question. ( Play Audio 18.04
+          (A) He is reminding students that they will have a test soon.
+          (B) He is checking to see if the students have any questions.
+          (C) He is preparing to introduce a new topic.
+          (D) He is pointing out information that will be on a test.""",
+        "audio_file": "listening/audio/18.04.ogg",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 3
+    },
+    {
+        "text": """8. What process results in the fossilization of soft tissue such as skin?
+          (A) permineralization
+          (B) trace fossil formation
+          (C) replacement and recrystalization
+          (D) molding and casting.""",
+        "audio_file": "",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 0
+    },
+    {
+        "text": """9. What is the professor probably going to talk about next?
+          (A) Additional examples of permineralization, molding, and casting
+          (B) The differences between trace fossils and body fossils
+          (C) How fossils form through recrystalization and replacement
+          (D) How permineralization works for trace fossils.""",
+        "audio_file": "",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 2
+    },
+
+    #Add answers!!!
+    {
+        "text": """10. According to the lecture, what statements characterize fossils created by permineralization or by casting and molding?
+          (A) Permineralization Casting and Molding
+          (B) Organism buried in wet environment
+          (C) Entire organism decays
+          (D) Dead organism buried under soil
+          (E) Minerals replace original cell walls.""",
+        "audio_file": "",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 2
+    },
+
+    #passage 3
+    {
+        "text": """11. What is the talk mainly about?
+          (A) What a medieval guild was and how it functioned
+          (B) The evolution of guilds into modern trade unions
+          (C) The different types of jobs medieval craftsmen had
+          (D) Why medieval craftsmen formed guilds.""",
+        "audio_file": "listening/audio/18.05.ogg",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 0
+    },
+    {
+        "text": """12. Why does the professor talk about Germany in the thirteenth century?
+          (A) To show how guilds differed in different parts of Europe
+          (B) To contrast agricultural life with town life at that time
+          (C) To give an example of how a mature guild functioned
+          (D) To illustrate the power of guilds over time.""",
+        "audio_file": "",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 2
+    },
+    {
+        "text": """13. Listen again to part of the lecture. Then answer the question. ( a Play Audio 18.06
+          (A) He thinks the students misunderstood his joke.
+          (B) He wants students to understand the job was considered important.
+          (C) He is checking to see his students' sense of humor.
+          (D) He is making sure his students are paying attention.""",
+        "audio_file": "listening/audio/18.06.ogg",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 1
+    },
+    {
+        "text": """14. According to the lecture, how did a young person start to learn a trade in medieval Europe?
+          (A) They learned it through schools.
+          (B) They attended university.
+          (C) They became an apprentice to a master craftsman.
+          (D) They got the guild's permission to be a journeyman.""",
+        "audio_file": "",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 2
+    },
+    {
+        "text": """15. Listen again to part of the lecture. Then answer the question. a 18.07
+          (A) He feels that the students are bored with the lecture.
+          (B) He is encouraging the students to ask more questions.
+          (C) He feels that his students need him to repeat information he just said.
+          (D) He is surprised that no one answered his question.""",
+        "audio_file": "listening/audio/18.07.ogg",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 3
+    },
+    {
+        "text": """16. According to the lecture, what did the journeyman need to find work?
+          (A) A set of his own tools
+          (B) Special papers from his guild
+          (C) A letter showing he was an apprentice
+          (D) Permission from a master craftsman.""",
+        "audio_file": "",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 1
+    },
+
+    #passage 4
+    {
+        "text": """17. What is the class discussion about?
+          (A) Classification of different agricultural revolutions
+          (B) Definition of fallow land and its role in 18th century Britain
+          (C) An evaluation of European farming practices
+          (D) Causes and effects of Britain's agricultural revolution""",
+        "audio_file": "listening/audio/18.08.ogg",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 3
+    },
+    {
+        "text": """18. Listen again to part of the lecture. Then answer the question. ( a Play Audio 18.09
+          (A) He wants to know if the students think the lecture is interesting.
+          (B) He is trying to find out if the students are paying attention.
+          (C) He wants the students to repeat the information he just said.
+          (D) He is checking to see if the students have any questions.""",
+        "audio_file": "listening/audio/18.09.ogg",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 3
+    },
+    {
+        "text": """19. Listen again to part of the lecture. Then answer the question. o Play Audio 18.10
+          (A) To clarify the student's misperception
+          (B) To show the student the correct pronunciation
+          (C) To repeat the student's ideas
+          (D) To support what the student just said.""",
+        "audio_file": "listening/audio/18.10.ogg",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 0
+    },
+    {
+        "text": """20. According to the lecture, how did Britons increase the land available for farming crops?
+          (A) They drained swampland and converted public grazing lands.
+          (B) They moved the population to cities to free up rural land.
+          (C) They reduced the size of private property holdings.
+          (D) They conquered new island territories.""",
+        "audio_file": "",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 0
+    },
+    {
+        "text": """21. What does the professor imply about allowing land to go fallow?
+          (A) It was a wasteful and foolish practice.
+          (B) It was not very important before the agricultural revolution.
+          (C) It was done to help the land to regain its nutrients.
+          (D) It was practiced by farmers without livestock.""",
+        "audio_file": "",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 3
+    },
+    #yes/no!!!!
+    {
+        "text": """22. Based on the information in the lecture, indicate whether the statements below were factors in the agricultural revolution.
+            Yes No
+          Connected raising livestock with growing land
+          Increased the acreage of fallow land
+          Combined small land plots into larger ones""",
+        "audio_file": "",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 2
+    },
+
+    #passage 5
+    {
+        "text": """23. Why does the student go to see the professor?
+          (A) To find out about a book she needs to read
+          (B) To seek clarification about a presentation
+          (C) To ask for advice on writing a paper
+          (D) To get advice on choosing a topic.""",
+        "audio_file": "listening/audio/18.11.ogg",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 1
+    },
+    {
+        "text": """24. Listen again to part of the conversation. Then answer the question. 6 a Play Audio 18.12
+          (A) He is not sure if he has met the student before.
+          (B) He doesn't want to student coming to his office so early.
+          (C) He is explaining why he forgot the student's name.
+          (D) He verifying the time of their appointment.""",
+        "audio_file": "listening/audio/18.12.ogg",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 2
+    },
+    {
+        "text": """25. According to the student, how does Huck Finn change in the novel ?
+          (A) He decides that he wants to make a lot of money.
+          (B) He treats the Grangerfords with kindness because of their wealth.
+          (C) He realizes it's wrong to cast negative judgments on strangers.
+          (D) He learns that that wealth does not automatically make someone a good person.""",
+        "audio_file": "",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 3
+    },
+    {
+        "text": """26. Listen again to part of the conversation. Then answer the question. ( Play Audio 18.13
+          (A) She feels rather certain about what she needs to do.
+          (B) She is very doubtful about what she needs to do.
+          (C) She wants the professor to read her draft.
+          (D) She thinks it is not written well enough.""",
+        "audio_file": "listening/audio/18.13.ogg",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 0
+    },
+
+    #add answers!!!!
+    {
+        "text": """27. According to the conversation, which of the features is part of each section of the presentation?
+          (A) Introduction B o d y Conclusion
+          (B) Briefly introduce supporting points
+          (C) State the main idea
+          (D) Summarize what was already said
+          (E) Talk about examples in detail.""",
+        "audio_file": "",
+        "options": ["A", "B", "C", "D", "E"],
+        "correct_answer": 2
+    },
+
+    #passage 6
+    {
+        "text": """28. What does the professor talk about in this lecture?
+          (A) She explains the features of hyperarid regions around the world
+          (B) She discusses how climate change can alter a desert
+          (C) She compares two different types of desert climates
+          (D) She explains how the center of the Arabian Desert formed""",
+        "audio_file": "listening/audio/18.13.ogg",
+        "options": ["A", "B", "C", "D"],
+        "correct_answer": 1
+    },
 ]
