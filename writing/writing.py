5a7fc1fa1f0898696238a11e57827c4f71e5df0f
Darya Zgirskaya <129535397+Antrsss@users.noreply.github.com>
2025-05-23 11:18:45 +0300

    writing_texts.py added

--- writing/writing.py
+++ writing/writing.py
@@ -1,62 +1,12 @@
 import asyncio
 import threading
 from telebot import types
-import os
+from .writing_texts import texts
 
 class WritingTest:
     def __init__(self, bot):
         self.bot = bot
-        self.tasks = [
-            {
-                "type": "integrated",
-                "reading_text": (
-                    "📖 *Writing Task 1 - Integrated*\n"
-                    "Read the passage about runic alphabet. (3 minutes)\n"
-                    "The runic alphabet is a set of letters made up of forms called runes." 
-                    "This writing system was used by various Germanic groups in central and" 
-                    "northern Europe as early as 150 AD until about 1100 AD. The origins of "
-                    "this alphabet have remained unclear, but archaeological evidence shows "
-                    "that runes most likely developed independently of any other alphabet. First, "
-                    "Germanic groups had little interaction with people from other parts of Europe "
-                    "until around 700 AD, when the Romans conquered many parts of central Europe. "
-                    "Because people in central and northern Europe were culturally isolated from "
-                    "other groups, it is unlikely that the runic alphabet was based on a previous "
-                    "writing system. Furthermore, according to the legends of some Germanic people, "
-                    "the alphabet was created by a mythological god called Odin. In one mythological "
-                    "text, Odin claims that the runes allow him to bring the dead back to life. The "
-                    "mythology of the Germanic groups was very different from the mythology in other parts of "
-                    "Europe, and the magical significance of the local alphabet within this mythology is "
-                    "also distinct. This implies that Germanic groups connected the runes to their unique "
-                    "mythology and traditions and not to writing systems from other groups. Finally, many archaeologists "
-                    "note that the runic alphabet lacks similarities to any other alphabet that existed before "
-                    "or at the same time as the runic system. Any similarities between the runic alphabet and "
-                    "other alphabets are most likely random or coincidental. This makes it difficult" 
-                    "to verify that the runic alphabet is a variant of an earlier alphabet."
-                ),
-                "audio_path": "writing/writing_task_1.ogg",
-                "audio_duration": 10,
-                "question": (
-                    "✍️ *Task:* Summarize the points made in the lecture, being sure to explain how they "
-                    "cast doubt on specific points made in the reading passage.\n\n"
-                    "⏳ *Time Limit:* 20 minutes\n"
-                    "📝 *Word Count:* 150-225 words"
-                ),
-                "reading_time": 10,
-                "writing_time": 10
-            },
-            {
-                "type": "discussion",
-                "prompt": (
-                    "💬 *Writing Task 2 - Discussion*\n"
-                    "Some people believe that university students should be required to attend classes. "
-                    "Others believe that going to classes should be optional for students.\n\n"
-                    "Which point of view do you agree with? Use specific reasons and examples to support your answer.\n\n"
-                    "⏳ *Time Limit:* 10 minutes\n"
-                    "📝 *Word Count:* ~120 words"
-                ),
-                "writing_time": 10
-            }
-        ]
+        self.tasks = texts
         self.active_users = {}
         self.current_task_index = {}
 
